5eb1a2366c00edc918ffc2465300e45af5e0794c
Rahmat <rahmatbola3@gmail.com>
2024-04-19 21:18:27 -0700

    route

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -9,16 +9,20 @@ from flask import Flask
 
 app = Flask(__name__)
 
+
 @app.route('/', strict_slashes=False)
 def index():
+
     """
     Root endpoint that returns a greeting message.
-    Accessible without strict slashes, meaning both / and / (trailing slash) will work.
+    Accessible without strict slashes, meaning both /
+    and / (trailing slash) will work.
     Returns:
         A string saying "Hello HBNB!"
     """
     return "Hello HBNB!"
 
-if __name__ == "__main__":
-    app.run(host='0.0.0.0', port=5000)
 
+if __name__ == "__main__":
+
+    app.run(host='0.0.0.0', port=5000)
